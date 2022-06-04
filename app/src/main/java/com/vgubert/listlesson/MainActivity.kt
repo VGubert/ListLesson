b5839240057ca4160625051fbf3980fd48e30d92
Vyacheslav <v.guber2000@gmail.com>
2022-06-05 00:12:02 +0300

    Lesson-3

--- app/src/main/java/com/vgubert/listlesson/MainActivity.kt
+++ app/src/main/java/com/vgubert/listlesson/MainActivity.kt
@@ -2,14 +2,44 @@ package com.vgubert.listlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.vgubert.listlesson.adapter.UserAdapter
 import com.vgubert.listlesson.databinding.ActivityMainBinding
+import com.vgubert.listlesson.model.UserModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var adapter: UserAdapter
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+    }
+    private fun init() {
+        recyclerView = binding.rvUser
+        adapter = UserAdapter(this)
+        recyclerView.adapter = adapter
+        adapter.setList(myUser())
     }
 
+    fun myUser(): ArrayList<UserModel> {
+        val userList = ArrayList<UserModel>()
+
+        val user = UserModel("Hetfield", "James")
+        userList.add(user)
+
+        val user2 = UserModel("Ulrich", "Lars")
+        userList.add(user2)
+
+        val user3 = UserModel("Hammett", "Kirk")
+        userList.add(user3)
+
+        val user4 = UserModel("Trujillo ", "Robert")
+        userList.add(user4)
+
+        return userList
+    }
 }
--- app/src/main/java/com/vgubert/listlesson/adapter/UserAdapter.kt
+++ app/src/main/java/com/vgubert/listlesson/adapter/UserAdapter.kt
@@ -1,17 +1,20 @@
 package com.vgubert.listlesson.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.vgubert.listlesson.R
 import com.vgubert.listlesson.model.UserModel
 import kotlinx.android.synthetic.main.item_user_layout.view.*
 
-class UserAdapter: RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val context: Context): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     private var userList = emptyList<UserModel>()
+
 
     class UserViewHolder(view: View): RecyclerView.ViewHolder(view)
 
@@ -23,6 +26,9 @@ class UserAdapter: RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         holder.itemView.tv_last_name.text = userList[position].lastName
         holder.itemView.tv_first_name.text = userList[position].firstName
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context,"$position", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
